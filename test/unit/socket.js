ea61efa9cf344b30374eeae06b07c91b4ab63cdd
Michael Christenson II <michael@rebelhold.com>
2014-05-01 15:21:16 -0700

    refactored out callbacks and made sockets listen for provider to be listening before gathering info like ip and port

--- test/unit/socket.js
+++ test/unit/socket.js
@@ -1,27 +1,106 @@
 var chai      = require('chai'),
     assert    = require('assert'),
+    restify   = require('restify'),
     app       = {},
     WebSocket = require('../../lib/web-socket');
 
 describe('webSocket', function () {
   it('listens to a server', function (done) {
-    app.restify = require('restify').createServer();
+    app.restify   = restify.createServer();
     var webSocket = new WebSocket(app);
     
-    webSocket.listen(app.restify, function (server) { 
-      assert(server instanceof Object);
-      assert(server.server);
-      assert(server.server instanceof Object);
-      assert(app.socket);
-      assert(app.socket == server);
+    app.socket = webSocket.listen(app.restify);
 
+    app.socket.on('listening', function () {
+      console.log('socket listening');
       done();
     });
+
+    app.restify.listen();
+  });
+
+  it('gets an ip', function (done) {
+    app.restify   = restify.createServer();
+    var webSocket = new WebSocket(app);
+
+    app.socket = webSocket.listen(app.restify);
+
+    app.socket.on('listening', function () {
+      assert(app.socket.ip);
+      assert(app.socket.ip === '127.0.0.1');
+      done();
+    });
+
+    app.restify.listen(null, '127.0.0.1');
+  });
+  
+  it('gets a port', function (done) {
+    app.restify   = restify.createServer();
+    var webSocket = new WebSocket(app);
+
+    app.socket = webSocket.listen(app.restify);
+
+    app.socket.on('listening', function () {
+      assert(app.socket.port);
+      assert(app.socket.port === 8080);
+      done();
+    });
+
+    app.restify.listen(8080);
   });
 
   describe('sockets', function () {
+    var webSocket = new WebSocket(app),
+        ioclient  = require('socket.io-client');
+
+    app.restify = restify.createServer();
+    app.socket  = webSocket.listen(app.restify);
+
+    /*
+    if(namespace instanceof Object) {
+      options   = namespace;
+      delete namespace;
+    }
+
+    var address = server.address(),
+        url     = [ 'ws://', address.address,
+          ':', address.port, (namespace || '') ].join();
+    
+    return ioclient(url, options);
+     */
+
     describe('on', function () {
-      it('connection');
+      describe('connection', function () {
+        /*
+        beforeEach(function () {
+          app.socket.sockets.on('connection', function (socket) {
+            app.socket.setup(socket);
+          });
+        });
+        */
+
+        /*
+        it('has an ip address', function (done) {
+          app.socket.on('listening', function () {
+            var incoming = client(app.socket, ioclient);
+            assert(incoming.ip);
+            assert(incoming.ip === app.socket.ip);
+            done();
+          });
+
+          app.restify.listen();
+        });
+        */
+
+        it('logs the event');
+
+        describe('rate limit', function () {
+          it('not exceeded');
+          it('exceeded');
+        });
+
+        it('mounts event listeners');
+      });
     });
 
     describe('socket', function () {
